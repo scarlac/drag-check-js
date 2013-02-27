bedeb4716b155e348f37758efc4a25c5db9930a0
Seph Soliman <seph@seph.dk>
2013-02-27 21:58:36 +0100

    added demo. reversed name

--- jquery.dragcheck.js
+++ jquery.dragcheck.js
@@ -2,32 +2,32 @@
  * Useful for lists where you user wants to select many intervals, but not entire table
  */
 (function($) {
-	$.fn.checkDrag = function() {
+	$.fn.dragCheck = function() {
 		// Remember the initial checkbox state
-		window.checkDrag_state = null;
+		window.dragCheck_state = null;
 		// Remember the initial checkbox (since click-drag outside-release action will not check initial checkbox)
-		window.checkDrag_origin = null;
+		window.dragCheck_origin = null;
 
 		this.mousedown(function() {
-			window.checkDrag_state = !this.checked;
-			window.checkDrag_origin = this;
+			window.dragCheck_state = !this.checked;
+			window.dragCheck_origin = this;
 		}).mouseup(function() {
-			window.checkDrag_state = null;
-			window.checkDrag_origin = null;
+			window.dragCheck_state = null;
+			window.dragCheck_origin = null;
 		}).mouseenter(function(e) {
 			// When dragging on a new checkbox, set it's state to the state of the initial checkbox
 			// Also set the initial checkbox' state to ensure it's checked when user finishes dragging
-			if(window.checkDrag_state !== null) {
-				$(this).add(window.checkDrag_origin)
-				.prop('checked', window.checkDrag_state)
+			if(window.dragCheck_state !== null) {
+				$(this).add(window.dragCheck_origin)
+				.prop('checked', window.dragCheck_state)
 				.trigger('change', e); // Force the event to trigger "change", so live feedback can be given
 			}
 		});
 
 		// Mouse release should disable the functionality
 		$(document.body).mouseup(function() { 
-			window.checkDrag_state = null;
-			window.checkDrag_origin = null;
+			window.dragCheck_state = null;
+			window.dragCheck_origin = null;
 		});
 	}
 })(jQuery);
